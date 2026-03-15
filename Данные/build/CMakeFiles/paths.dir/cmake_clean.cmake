16ce88f97dba16323c644f0b8fb52c9bedb091eb
Mihail <iceplay@LAPTOP-QJNQQ19R.localdomain>
2026-03-15 22:15:28 +0300

    Удалить venv и Данные/build из индекса

--- Данные/build/CMakeFiles/paths.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/paths"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/paths.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
